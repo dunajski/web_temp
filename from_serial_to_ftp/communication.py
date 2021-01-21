c7958487cc45a423034a47ea4438c6b11bebdd16
Michal Dunajski <michaldunajski2505@gmail.com>
2021-01-21 22:38:30 +0100

    Script prepared to read from serial
    Be careful and properly configure to serial port in your PC.

--- from_serial_to_ftp/communication.py
+++ from_serial_to_ftp/communication.py
@@ -5,18 +5,25 @@
 # PySerial docs https://pyserial.readthedocs.io/en/latest/shortintro.html
 import serial
 
-def readfromserial():
+serialString = ""
+
+
+if __name__ == "__main__":
     # check port name with device manager (windows) or
     # dmesg | grep tty (linux)
     # port_name = "COM2"
-    # port_name = "tty/USB0"
+    port_name = '/dev/ttyUSB0'
     serialPort = serial.Serial(port=port_name, baudrate=9600, bytesize=8,
                                parity='N', stopbits=1, timeout=2)
     print("Reading from serial"  + serialPort.name +  " started....")
-    # TODO reading from serial
 
-if __name__ == "__main__":
-    readfromserial()
+    while(1):
 
+        # Wait until there is data waiting in the serial buffer
+        if(serialPort.in_waiting > 0):
 
+            # Read data out of the buffer until a carraige return / new line is found
+            serialString = serialPort.readline()
 
+            # Print the contents of the serial data
+            print(serialString.decode('Ascii'))
